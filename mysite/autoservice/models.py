f85b5daf211cf770b3397da6e2c8d922b9cdaaff
VytautasZ12 <vzvytautasz@gmail.com>
2023-08-29 11:37:09 +0300

    Added str methods

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -6,6 +6,9 @@ class AutomobilioModelis(models.Model):
     marke = models.CharField(verbose_name="Marke", max_length=100)
     modelis = models.CharField(verbose_name="Modelis", max_length=100)
 
+    def __str__(self):
+        return f"{self.marke} {self.modelis}"
+
 
 class Automobilis(models.Model):
     valst_nr = models.CharField(verbose_name="Valstybinis numeris", max_length=20)
@@ -14,10 +17,16 @@ class Automobilis(models.Model):
     automobilio_modelis = models.ForeignKey(to="AutomobilioModelis", verbose_name="Modelis", on_delete=models.SET_NULL,
                                             null=True)
 
+    def __str__(self):
+        return f"{self.valst_nr} - {self.vin_kodas}"
+
 
 class Uzsakymas(models.Model):
     data = models.DateField(verbose_name="Data", auto_now_add=True)
     automobilis = models.ForeignKey(to="Automobilis", verbose_name="Automobilis", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.data}({self.automobilis})"
 
 
 class UzsakymoEilute(models.Model):
@@ -25,7 +34,13 @@ class UzsakymoEilute(models.Model):
     paslauga = models.ForeignKey(to="Paslauga", verbose_name="Paslauga", on_delete=models.SET_NULL, null=True)
     kiekis = models.IntegerField(verbose_name="Kiekis")
 
+    def __str__(self):
+        return f"{self.uzsakymas} ({self.paslauga} - {self.kiekis})"
+
 
 class Paslauga(models.Model):
     pavadinimas = models.CharField(verbose_name="Paslaugos pavadinimas", max_length=50)
     kaina = models.FloatField(verbose_name="Kaina")
+
+    def __str__(self):
+        return f"{self.pavadinimas} ({self.kaina})"
